3e8defeeaa04cf931d21b9e7796c839b33c171b5
fuadariqoh <fuadariqoh@gmail.com>
2020-06-14 13:00:40 +0800

    grid

--- src/pages/TransactionHistory/TransactionHistory.jsx
+++ src/pages/TransactionHistory/TransactionHistory.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import styles from './TransactionHistory.module.css'
+import React from "react";
+import styles from "./TransactionHistory.module.css";
+import { Container, Row, Col } from "reactstrap";
 
-const TransactionHistory=()=>{
+const TransactionHistory = () => {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>TEST TransactionHistory</Col>
+          <Col>asdasdasd</Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className={styles.descriptionbox}>
+              <div>TEST</div>
+              <div>asdasd</div>
+              <div>asdasdas</div>
+            </div>
+          </Col>
+          <Col>ASDASDASD</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-
-    return(
-        <div className={styles.container}>
-                    <div>
-                        <h1>TransactionHistory</h1>
-
-                    </div>
-        </div>
-    )
-}
-
-
-export default TransactionHistory
+export default TransactionHistory;
