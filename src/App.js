b9e79cf1da437a2d671769940858dffcea3c9e84
fuadariqoh <fuadariqoh@gmail.com>
2020-06-29 00:12:46 +0800

    Merge branch 'fuad'

--- src/App.js
+++ src/App.js
@@ -1,56 +1,56 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import { Route, Switch} from "react-router-dom";
-import { connect } from 'react-redux';
-import Axios from 'axios';
-import Login from './pages/Auth/Login'
-import Register from './pages/Auth/Register'
-import RegisterEmailVerify from './pages/Auth/RegisterEmailVerify'
-import Notfound from './pages/NotFound'
-import Home from './pages/Home'
-import ManageUser from './pages/ManageUser/ManageUser'
-import TransactionHistory from './pages/TransactionHistory/TransactionHistory'
-import Verified from './pages/Auth/Verified'
-import ForgotPassword from './pages/Auth/ForgotPassword'
-import ResetPassword from './pages/Auth/ResetPassword'
-import ManageTransaksi from './pages/ManageTransaksi/ManageTransaksi';
-import Reward from './pages/Reward/Reward';
-import Dashboard from './pages/Dashboard/Dashboard';
-import Report from './pages/Report/Report';
-import Program from './pages/Program/ProgramPage'
-import ProgramDetail from './pages/Program/ProgramDetail'
-import JoinProgram from './pages/Program/JoinProgram'
-import ScrollToTop from './components/scrolltop'
-import {KeepLogin} from './redux/actions'
-import { API_URL } from './support/Apiurl'
-import TransactionDetail from './pages/Program/TransactionDetail'
-import NavBar from './components/NavBar/NavBar'
-import Footer from './components/Footer/footer'
-import ContactUs from './pages/ContactUs/contactus';
-import AboutUs from './pages/AboutUs/aboutus'
-import CollectionPrograms from './pages/CollectionPrograms/CollectionPrograms';
-import MyImpact from './pages/MyImpact/MyImpact'
-import PersonalInfo from './pages/PersonalInfo/PersonalInfo';
-import EditProfile from './pages/EditProfile/EditProfile';
+// import React, {useState, useEffect} from 'react';
+import "./App.css";
+// import { Route, Switch} from "react-router-dom";
+// import { connect } from 'react-redux';
+// import Axios from "axios";
+// import Login from './pages/Auth/Login'
+// import Register from './pages/Auth/Register'
+// import RegisterEmailVerify from './pages/Auth/RegisterEmailVerify'
+// import Notfound from './pages/NotFound'
+// import Home from './pages/Home'
+// import ManageUser from './pages/ManageUser/ManageUser'
+// import TransactionHistory from './pages/TransactionHistory/TransactionHistory'
+// import Verified from './pages/Auth/Verified'
+// import ForgotPassword from './pages/Auth/ForgotPassword'
+// import ResetPassword from './pages/Auth/ResetPassword'
+// import ManageTransaksi from './pages/ManageTransaksi/ManageTransaksi';
+// import Reward from './pages/Reward/Reward';
+// import Dashboard from './pages/Dashboard/Dashboard';
+// import Report from './pages/Report/Report';
+// import Program from './pages/Program/ProgramPage'
+// import ProgramDetail from './pages/Program/ProgramDetail'
+// import JoinProgram from './pages/Program/JoinProgram'
+// import ScrollToTop from './components/scrolltop'
+// import {KeepLogin} from './redux/actions'
+// import { API_URL } from "./support/Apiurl";
+// import TransactionDetail from './pages/Program/TransactionDetail'
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/footer";
+import ContactUs from "./pages/ContactUs/contactus";
+import AboutUs from "./pages/AboutUs/aboutus";
+import CollectionPrograms from "./pages/CollectionPrograms/CollectionPrograms";
+import MyImpact from "./pages/MyImpact/MyImpact";
+import PersonalInfo from "./pages/PersonalInfo/PersonalInfo";
+import EditProfile from "./pages/EditProfile/EditProfile";
 
-function App({KeepLogin}) {
+// function App({KeepLogin}) {
 
-  const [Loading, setLoading]=useState(true)
+//   const [Loading, setLoading]=useState(true)
 
-  useEffect(()=>{
-    var token = localStorage.getItem('token')
-    if(token){
-      Axios.get(`${API_URL}/users/keeplogin`,{
-        headers:{
-          'Authorization' :  `Bearer ${token}`
-        }
-      })
-      .then(res=>{
-        console.log(res.data)
-        KeepLogin(res.data)
-      })
-      .catch(err=>{
-        console.log(err.message)
+//   useEffect(()=>{
+//     var token = localStorage.getItem('token')
+//     if(token){
+//       Axios.get(`${API_URL}/users/keeplogin`,{
+//         headers:{
+//           'Authorization' :  `Bearer ${token}`
+//         }
+//       })
+//       .then(res=>{
+//         console.log(res.data)
+//         KeepLogin(res.data)
+//       })
+//       .catch(err=>{
+//         console.log(err.message)
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
@@ -118,9 +118,9 @@ function App({ KeepLogin }) {
 
   return (
     <div>
-      <ScrollToTop/>
+      <ScrollToTop />
 
-      <NavBar/>
+      <NavBar />
 
       <ScrollToTop />
       <Switch>
@@ -140,16 +140,28 @@ function App({ KeepLogin }) {
         <Route path="/cart/:userid" component={Cart} exact />
         <Route path="/rewardcheckout" component={RewardCheckout} exact />
         <Route path="/dashboard" component={Dashboard} exact />
-        <Route path="/transactionhistory" component={TransactionHistory} exact />
-        <Route path="/programdetail/:idprog" exact component={ProgramDetail}/>
-        <Route path="/joinprogram/:idprog" exact component={JoinProgram}/>
-        <Route path="/transactiondetail/:idtrans" exact component={TransactionDetail}/>
-        <Route path='/collection-programs' component={CollectionPrograms} exact />
-        <Route path='/my-impact' component={MyImpact} exact />
-        <Route path='/personal-info' component={PersonalInfo} exact />
-        <Route path='/edit-profile' component={EditProfile} exact />
-        <Route  path='/aboutus' component={AboutUs} exact />
-        <Route  path='/contactus' component={ContactUs} exact />
+        <Route
+          path="/transactionhistory"
+          component={TransactionHistory}
+          exact
+        />
+        <Route path="/programdetail/:idprog" exact component={ProgramDetail} />
+        <Route path="/joinprogram/:idprog" exact component={JoinProgram} />
+        <Route
+          path="/transactiondetail/:idtrans"
+          exact
+          component={TransactionDetail}
+        />
+        <Route
+          path="/collection-programs"
+          component={CollectionPrograms}
+          exact
+        />
+        <Route path="/my-impact" component={MyImpact} exact />
+        <Route path="/personal-info" component={PersonalInfo} exact />
+        <Route path="/edit-profile" component={EditProfile} exact />
+        <Route path="/aboutus" component={AboutUs} exact />
+        <Route path="/contactus" component={ContactUs} exact />
         <Route path="/report" component={Report} exact />
         <Route path="/managetransaksi" component={ManageTransaksi} exact />
         <Route path="/manageuser" component={ManageUser} exact />
@@ -175,10 +187,9 @@ function App({ KeepLogin }) {
         <Route component={Notfound} />
       </Switch>
 
-      <Footer/>
-
+      <Footer />
     </div>
   );
 }
 
-export default connect(null, { KeepLogin })(App)
+export default connect(null, { KeepLogin })(App);
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import logoheader from './../../images/recyly_nobg.png'
+import React, { useState } from "react";
+import logoheader from "./../../images/recyly_nobg.png";
 import {
   Collapse,
   Navbar,
@@ -12,43 +12,53 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  NavbarText
-} from 'reactstrap';
-import './NavBar.css'
-import {FaSearch, FaHandHoldingHeart} from 'react-icons/fa'
-import {connect} from 'react-redux'
+  NavbarText,
+} from "reactstrap";
+import "./NavBar.css";
+import { FaSearch, FaHandHoldingHeart } from "react-icons/fa";
+import { connect } from "react-redux";
 
 const NavBar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [search,setSearch] = useState({
-    searchInput:''
-  })
+  const [search, setSearch] = useState({
+    searchInput: "",
+  });
 
   const toggle = () => setIsOpen(!isOpen);
 
   //Onchange untuk Search Bar
-  const onChangeSearch=(e)=>{
-    setSearch({...search,[e.target.name]:e.target.value})
-    console.log(search.searchInput)
-  }
-    
+  const onChangeSearch = (e) => {
+    setSearch({ ...search, [e.target.name]: e.target.value });
+    console.log(search.searchInput);
+  };
+
   return (
     <div>
       <Navbar color="white" light expand="md">
-      <NavbarBrand href="/">
-      <img fixed='top' src={logoheader} alt='logo' height='45px' style={{paddingLeft:'50px'}}></img>
-      </NavbarBrand>
-        
+        <NavbarBrand href="/">
+          <img
+            fixed="top"
+            src={logoheader}
+            alt="logo"
+            height="45px"
+            style={{ paddingLeft: "50px" }}
+          ></img>
+        </NavbarBrand>
+
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink className='headernavlink' href="/program">ALL PROGRAM</NavLink>
+              <NavLink className="headernavlink" href="/program">
+                ALL PROGRAM
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className='headernavlink' href="/reward">REWARD</NavLink>
+              <NavLink className="headernavlink" href="/reward">
+                REWARD
+              </NavLink>
             </NavItem>
-            <UncontrolledDropdown className='headernavlink' nav inNavbar>
+            <UncontrolledDropdown className="headernavlink" nav inNavbar>
               <DropdownToggle nav caret>
                 ABOUT
               </DropdownToggle>
@@ -62,52 +72,67 @@ const NavBar = (props) => {
               </DropdownMenu>
             </UncontrolledDropdown>
           </Nav>
-          
-          <div className='search-container'>
+
+          <div className="search-container">
             <div>
-                <input type="text" name='searchInput' onChange={onChangeSearch}/> &nbsp; 
-                <a href='/search'>
-                <button style={{width:'35px', border:'black', borderRadius:'30px'}}><FaSearch/></button>
-                </a>
+              <input type="text" name="searchInput" onChange={onChangeSearch} />{" "}
+              &nbsp;
+              <a href="/search">
+                <button
+                  style={{
+                    width: "35px",
+                    border: "black",
+                    borderRadius: "30px",
+                  }}
+                >
+                  <FaSearch />
+                </button>
+              </a>
             </div>
           </div>
-
         </Collapse>
       </Navbar>
 
-      {
-          props.User.isLogin?
-          <div className='headermenu-containerislogin'>
-              <div className='headermenu-item'>
-                  <a href='/collection-programs' style={{color:'inherit'}}>PROFILE</a>
-              </div>
-              <div className='headermenu-item'>
-                  <a href='/home' style={{color:'inherit'}}>SIGN OUT</a>
-              </div>
-              <div className='headermenu-itemlast'>
-                  <a href='/cart' style={{color:'inherit'}}><FaHandHoldingHeart className='holdingheart'/> (0)</a>
-              </div>
+      {props.User.isLogin ? (
+        <div className="headermenu-containerislogin">
+          <div className="headermenu-item">
+            <a href="/collection-programs" style={{ color: "inherit" }}>
+              PROFILE
+            </a>
           </div>
-           :
-           <div className='headermenu-container'>
-               <div className='headermenu-item'>
-                   <a href='/register' style={{color:'inherit'}}>SIGN UP</a>
-               </div>
-               <div className='headermenu-itemlast'>
-                   <a href='/login' style={{color:'inherit'}}>SIGN IN</a>
-               </div>
-           </div>
-         }
-
+          <div className="headermenu-item">
+            <a href="/home" style={{ color: "inherit" }}>
+              SIGN OUT
+            </a>
+          </div>
+          <div className="headermenu-itemlast">
+            <a href={`/cart/${props.User.id}`} style={{ color: "inherit" }}>
+              <FaHandHoldingHeart className="holdingheart" /> CART
+            </a>
+          </div>
+        </div>
+      ) : (
+        <div className="headermenu-container">
+          <div className="headermenu-item">
+            <a href="/register" style={{ color: "inherit" }}>
+              SIGN UP
+            </a>
+          </div>
+          <div className="headermenu-itemlast">
+            <a href="/login" style={{ color: "inherit" }}>
+              SIGN IN
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-const MapstatetoProps=(state)=>{
-  return{
-      User:state.Auth,
-  }
-}
+const MapstatetoProps = (state) => {
+  return {
+    User: state.Auth,
+  };
+};
 
-
-export default connect(MapstatetoProps,{})(NavBar);
+export default connect(MapstatetoProps, {})(NavBar);
